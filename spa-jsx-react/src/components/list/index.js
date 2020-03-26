e5aad40f39bcd8f1247b6e8f96df6537f43d5120
PaulinaLL <paulinalll90@wp.pl>
2020-03-26 12:11:39 +0100

    create List function

--- spa-jsx-react/src/components/list/index.js
+++ spa-jsx-react/src/components/list/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-
+import data from "../../data/data.json";
+import ListItem from "../listItem";
 function List() {
-    return (
-
-
-
-
-
-
-
-    )
+  return (
+    <ul>
+      {data.map((person, index) => {
+        return <ListItem key={index} data={person} />;
+      })}
+    </ul>
+  );
 }
 
 export default List;
